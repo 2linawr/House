9da221e8ba767d84ef3bfec5bfbb044462ba829e
2linawr <61121260+2linawr@users.noreply.github.com>
2022-09-04 16:39:10 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,51 @@
+window.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call( document.querySelectorAll('#phone'), function(input) {
+    var keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7(___)___-__-__",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
+});
+
+function Send(){
+    const inputTel = document.querySelector('#phone')
+    let msg
+    inputTel.addEventListener('focusout', event=>{
+        let length = inputTel.value.length
+        if (length > 12) msg = 'Количество символов не должно превышать 12!'
+        else if (length < 16) msg = 'Количество символов не должно быть меньше 11!'
+        alert(msg);
+    })
+
+$.post( "mail(php).php", { name: "John", time: "2pm" },function(data){
+alert('Данные отправились');
+} );
+}
